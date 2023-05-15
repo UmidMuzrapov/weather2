50f2fc3a4681017acf366b3ef8fb7c11388f5b74
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2023-05-14 20:51:12 -0700

    test is alright

--- Weather/Server/Controllers/WeatherForecastController.cs
+++ Weather/Server/Controllers/WeatherForecastController.cs
@@ -1,8 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Weather.Shared;
 
 namespace Weather.Server.Controllers
