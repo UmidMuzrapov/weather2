8250f75989a0f2f01b38773d65f6f204e800f438
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2023-05-14 20:12:26 -0700

    Implement WeatherService

---
+++ Weather/Client/wwwroot/Services/Interfaces/IWeatherService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Weather.Shared.Models.Main;
+
+namespace Weather.Client.Services
+{
+    public interface IWeatherService
+    {
+        public Weather.Shared.Models.Main.Weather? GetWeather();
+
+        public Task SetWeather();
+
+        public void SertDependencies(HttpClient http, NavigationManager navigation);
+
+        public Location? GetLocation();
+
+        public void SetLocation(Location location);
+
+        public List<DayModel> GetDaysInfo();
+
+    }
+}
---
+++ Weather/Client/wwwroot/Services/WeatherService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Weather.Shared.Models.Main;
+
+namespace Weather.Client.Services
+{
+    
+
+    public class WeatherService : IWeatherService
+    {
+        private HttpClient _httpClient;
+        private NavigationManager _navigationManager;
+        private List<DayModel> _days;
+
+        private Weather.Shared.Models.Main.Weather? weather { get; set; }
+        private Location? Location { get; set; }
+
+        public Weather.Shared.Models.Main.Weather? GetWeather()
+        {
+            return weather;
+        }
+
+        public async Task SetWeather()
+        {
+            var response = await _httpClient.PostAsJsonAsync(_navigationManager.BaseUri+"api/weather", Location);
+
+            if (response.IsSuccessStatusCode)
+            {
+                weather=await response.Content.ReadFromJsonAsync<Weather.Shared.Models.Main.Weather?>();
+                CreateListOfDays();
+            }
+
+        }
+
+        public void SertDependencies(HttpClient http, NavigationManager navigation)
+        {
+            _httpClient=http;
+            _navigationManager=navigation;
+        }
+
+        public void SetLocation(Location location)
+        {
+            Location= location;
+        }
+
+        public Location? GetLocation()
+        {
+            return Location;
+        }
+
+        public List<DayModel> GetDaysInfo()
+        {
+            return _days;
+        }
+
+        private void CreateListOfDays()
+        {
+            _days = new List<DayModel>();
+            for (int i=0; i<weather.WeatherDaily.time.Length; i++)
+            {
+                _days.Add (new DayModel
+                {
+                    time=weather.WeatherDaily.time[i],
+                    temperature_2m_max=weather.WeatherDaily.temperature_2m_max[i],
+                    temperature_2m_min=weather.WeatherDaily.temperature_2m_min[i],
+                    sunrise=weather.WeatherDaily.sunrise[i],
+                    sunset=weather.WeatherDaily.sunset[i],
+                    uv_index=weather.WeatherDaily.uv_index_max[i],
+                    precipitation_hours=weather.WeatherDaily.precipitation_hours[i],
+                    windspeed_10m_max=weather.WeatherDaily.windspeed_10m_max[i],
+                });
+            }
+        }
+    }
+}
